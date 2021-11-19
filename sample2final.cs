9f00a1f1ad28d31a435b6b489efa5e5fe824050b
micaleangel <92702174+micaleangel@users.noreply.github.com>
2021-11-19 20:06:46 +0800

    Create sample2final.cs

---
+++ sample2final.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+
+ public static void Main()
+{
+
+ string names "Micale";
+ int age = 20;
+ Console.WriteLine("Hi" + name); 
+ Console.WriteLine("Your age is "+ age);
+   }
+}
