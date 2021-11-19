19aa5327f26fd959d7c543f4298d091a09aaf487
micaleangel <92702174+micaleangel@users.noreply.github.com>
2021-11-19 20:29:26 +0800

    Create sample4.cs

---
+++ sample4.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Program
+ {
+
+public static void Main()
+
+{
+
+   int x = 7;
+   int y = 27; 
+   int temp = x;
+
+   x = y; 
+   y = temp;
+
+    Console.WriteLine("The value of x is " + x); 
+    Console.WriteLine("The value of y is " + y);
+ }
+
+}
