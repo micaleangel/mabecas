5f183b9d886d446acb010eb869a15c22faefc38f
micaleangel <92702174+micaleangel@users.noreply.github.com>
2021-11-19 20:25:17 +0800

    Create sample3.cs

---
+++ sample3.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program
+
+ {
+
+public static void Main()
+{
+
+  string fname = "Micale":
+  string Iname = "Becas":
+  Console.WriteLine(fname + " " Iname);
+}
+
+}
